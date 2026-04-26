076bef0c01e8800e3c64451120f4b8f1e69c4e16
NoiSeandSmokee <ngominhhoang1815@gmail.com>
2026-04-26 19:58:57 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -19,3 +19,33 @@ showOnScroll();
 function goBack(){
     window.location = "index.html";
 }
+
+
+const items = document.querySelectorAll(".accordion .item");
+
+items.forEach(item => {
+    item.querySelector(".title").addEventListener("click", () => {
+
+        // đóng tất cả trước
+        items.forEach(i => {
+            if (i !== item) i.classList.remove("active");
+        });
+
+        // mở cái đang click
+        item.classList.toggle("active");
+    });
+});
+
+document.getElementById("searchBook").addEventListener("keyup", function () {
+    let keyword = this.value.toLowerCase();
+
+    let books = document.querySelectorAll(".accordion .content p");
+
+    books.forEach(book => {
+        if (book.innerText.toLowerCase().includes(keyword)) {
+            book.style.display = "block";
+        } else {
+            book.style.display = "none";
+        }
+    });
+});
